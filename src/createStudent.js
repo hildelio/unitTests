622e098553b435ab407d38d53f760682868f2e1f
hildelio <hildelio@gmail.com>
2022-07-22 09:57:16 -0300

    Requisito 6 cumprido e indentado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = () => {
+//   let feedback = 'a';
+//   return student = [{Nome: name}, {Feedback: feedback}]
+//   student.Feedback = feedback = () => 'Eita pessoa boa!';
+};
 
 module.exports = createStudent;
